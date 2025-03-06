1ef9a05cff6274bc076fea1268a313d2daa4b130
khushboo kanojiya <khushboo@marketsmojo.com>
2025-03-06 11:48:43 +0530

    Initial commit

---
+++ reactnew/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ReactIntro from "./components/ReactIntro.jsx";
+
+function App() {
+
+
+  return (
+    <div>
+      <ReactIntro />
+    </div>
+
+  )
+}
+
+export default App
---
+++ reactnew/src/components/ReactIntro.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const Login = () => {
+  // State to store user input
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Function to handle login
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Logging in with:", email, password);
+    // Call API or authentication logic here
+  };
+
+  return (
+    <div >
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input style={{ margin: "10px" }}
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input style={{ margin: "10px" }}
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button style={{ margin: "10px" }} type="submit">Login</button>
+      </form>
+
+      <h2 className="pt-5">List in react js</h2>
+    </div>
+  );
+
+};
+
+export default Login;
