89f3828130a67d110532ca5dd9c64af90ea88848
khushboo kanojiya <khushboo@marketsmojo.com>
2025-03-06 16:25:44 +0530

    changes done

--- reactnew/src/components/ReactIntro.jsx
+++ reactnew/src/components/ReactIntro.jsx
@@ -1,40 +1,33 @@
-import { useState } from "react";
+  import { useState } from "react";
+  const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleLogin = (e) => {
+      e.preventDefault();
+      console.log("Logging in with:", email, password);
+    };
+    return (
+      <div >
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <input style={{ margin: "10px"}}
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input style={{ margin: "10px"}}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button style={{ margin: "10px" }} type="submit">Login</button>
+        </form>
+        <h2 className="pt-5" style={{ padding: '20px' }}>my list ar decided </h2>
 
-const Login = () => {
-  // State to store user input
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  // Function to handle login
-  const handleLogin = (e) => {
-    e.preventDefault();
-    console.log("Logging in with:", email, password);
-    // Call API or authentication logic here
+      </div>
+    );
   };
 
-  return (
-    <div >
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input style={{ margin: "10px" }}
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input style={{ margin: "10px" }}
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button style={{ margin: "10px" }} type="submit">Login</button>
-      </form>
-
-      <h2 className="pt-5" style={{ padding: '20px' }}>List in react js learn</h2>
-    </div>
-  );
-
-};
-
-export default Login;
+  export default Login;
