923f265aa5cce6e2d664b4500f75458502d7bc5d
khushboo kanojiya <khushboo@marketsmojo.com>
2025-03-07 16:33:01 +0530

    changes done

--- reactnew/src/components/ReactIntro.jsx
+++ reactnew/src/components/ReactIntro.jsx
@@ -1,33 +1,33 @@
-  import { useState } from "react";
-  const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const handleLogin = (e) => {
-      e.preventDefault();
-      console.log("Logging in with:", email, password);
-    };
-    return (
-      <div >
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-          <input style={{ margin: "10px"}}
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input style={{ margin: "10px"}}
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button style={{ margin: "10px" }} type="submit">Login</button>
-        </form>
-        <h2 className="pt-5" style={{ padding: '20px' }}>my list ar decided </h2>
-
-      </div>
-    );
+import { useState } from "react";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Logging in with:", email, password);
   };
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input style={{ margin: "10px" }}
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input style={{ margin: "10px" }}
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        
+        <button style={{ margin: "10px" }} type="submit">Login</button>
+      </form>
+      <h2 className="pt-5" style={{ padding: '20px' }}> My list are decided </h2>
+    </div>
+  );
+};
 
-  export default Login;
+export default Login;
